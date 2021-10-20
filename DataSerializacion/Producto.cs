331046e44787e057f4da4a0ffad28616924a3c00
Luis Elías Carro <luis.carro@gmail.com>
2021-10-20 13:38:33 -0300

    agregado JSON

---
+++ DataSerializacion/Producto.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Text;
+
+namespace DataSerializacion
+{
+    public class Producto
+    {
+        public string Nombre { set; get; }
+        public DateTime Vencimiento { set; get; }
+        public double Precio { set; get; }
+        public string[] Calidades { set; get; }
+
+        public string GetDatos()
+        {
+            var datos = new StringBuilder();
+
+            datos.Append($"Nombre: {Nombre}{Environment.NewLine}");
+            datos.Append($"Vencimiento: {Vencimiento:d}{Environment.NewLine}");
+            datos.Append($"Precio: {Precio:F}{Environment.NewLine}");
+            datos.Append($"Calidades: {string.Join(",", Calidades)}{Environment.NewLine}");
+
+            return datos.ToString();
+        }
+    }
+}
---
+++ TiposSerializacion/SerializacionJson.cs
@@ -0,0 +1,35 @@
+﻿using System.IO;
+using System.Text;
+using DataSerializacion;
+using Newtonsoft.Json;
+
+namespace TiposSerializacion
+{
+    public class SerializacionJson
+    {
+        public void Serializar(Producto p, string archivo)
+        {
+            string ps = JsonConvert.SerializeObject(p);
+
+            using (Stream fs = new FileStream(archivo, FileMode.Create, FileAccess.Write, FileShare.None))
+            {
+                byte[] bytes = Encoding.UTF8.GetBytes(ps);
+                fs.Write(bytes, 0, bytes.Length);
+            }
+        }
+
+        public Producto Deserializar(string archivo)
+        {
+            using (Stream fs = new FileStream(archivo, FileMode.Open, FileAccess.Read, FileShare.None))
+            {
+                using (var sr = new StreamReader(fs))
+                {
+                    var data = sr.ReadToEnd();
+                    Producto p = JsonConvert.DeserializeObject<Producto>(data);
+
+                    return p;
+                }
+            }
+        }
+    }
+}
